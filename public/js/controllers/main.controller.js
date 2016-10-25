1acf55d42b3a2692c158da6487773b13cfb6a0a1
AnuThogara <anuradha.thogara@gmail.com>
2016-10-24 20:38:30 -0400

    handled duplications and implemented update functionality

--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -6,13 +6,21 @@
 
 MainController.$inject = ['$scope', 'TodoService'];
 function MainController($scope, TodoService){
-  console.log(TodoService.get());
-  TodoService.create('make more todos');
-  console.log(TodoService.get());
-  TodoService.update(0, 'buy something');
-  console.log(TodoService.get());
-  TodoService.delete(2);
-  console.log(TodoService.get());
+  // console.log(TodoService.get());
+  // TodoService.create('make more todos');
+  // console.log(TodoService.get());
+  // TodoService.update(0, 'buy something');
+  // console.log(TodoService.get());
+  // TodoService.delete(2);
+  // console.log(TodoService.get());
+  $scope.todos =TodoService.get();
+  $scope.createTodo = createTodo;
+
+  function createTodo(newTodo){
+    TodoService.create(newTodo);
+    $scope.newTodo = '';
+  }
+
 
 
 }
